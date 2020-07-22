e854f05dd8ebc50a15484e0a0136f3ca4d7b961f
Scott Stornetta <scottstornetta@gmail.com>
2020-07-22 16:29:55 +0000

    also needed to add modified files :(

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <button class="btn btn-primary">Is bootstrap working?</button>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
