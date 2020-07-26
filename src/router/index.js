c3cbb72c3cd774a7ec849c44c7fa295ddae861d2
Scott Stornetta <scottstornetta@gmail.com>
2020-07-26 02:40:40 +0000

    routes working basic

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/authentication/Login.vue'
+import Register from '../views/authentication/Register.vue'
+import TasksAll from '../views/tasks/TasksAll.vue'
+import TasksCreate from '../views/tasks/TasksCreate.vue'
+import TasksEdit from '../views/tasks/TasksEdit.vue'
+
+
+
 
 Vue.use(VueRouter)
 
@@ -11,13 +19,35 @@ Vue.use(VueRouter)
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/tasks',
+    name: 'tasks-all',
+    component: TasksAll
+  },
+  {
+    path: '/tasks/new',
+    name: 'tasks-create',
+    component: TasksCreate
+  },
+  {
+    path: '/tasks/:id',
+    name: 'tasks-edit',
+    component: TasksEdit
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: Register
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '*',
+    redirect: '/'
   }
+
 ]
 
 const router = new VueRouter({
