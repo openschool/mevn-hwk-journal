df7c147b9b5d3a8b9f63ce63f3dcfa586c8923e3
Scott Stornetta <scottstornetta@gmail.com>
2020-08-12 17:38:55 +0000

    not yet running, but all code up to lesson 18

--- dev-server/index.js
+++ dev-server/index.js
@@ -5,9 +5,18 @@ const app = express()
 const port = 3004
 
 import {registerRoutes} from './routes';
+import {setEnvironment} from './config/env';
 
+setEnvironment(app);
 registerRoutes(app);
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+    if(process.env.NODE_ENV !== 'production'){
+    return res.send('Running server in development mode')
+    } else {
+        return res.sendFile('index.html',{root: __dirname + '/../dist'});
+    }
 
-app.listen(port, () => console.log(`journal app listening at http://localhost:${port}`))
+})
+
+app.listen(port, () => console.log(`journal app listening at http://localhost:${port} in ` + process.env.NODE_ENV + ' mode!'));
--- prod-server/index.js
+++ prod-server/index.js
@@ -6,6 +6,8 @@ var _express2 = _interopRequireDefault(_express);
 
 var _routes = require('./routes');
 
+var _env = require('./config/env');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var app = (0, _express2.default)(); // below is same as const express = require('express')
@@ -13,12 +15,17 @@ var app = (0, _express2.default)(); // below is same as const express = require(
 
 var port = 3004;
 
+(0, _env.setEnvironment)(app);
 (0, _routes.registerRoutes)(app);
 
 app.get('/', function (req, res) {
-  return res.send('Hello World!');
+    if (process.env.NODE_ENV !== 'production') {
+        return res.send('Running server in development mode');
+    } else {
+        return res.sendFile('index.html', { root: __dirname + '/../dist' });
+    }
 });
 
 app.listen(port, function () {
-  return console.log('journal app listening at http://localhost:' + port);
+    return console.log('journal app listening at http://localhost:' + port + ' in ' + process.env.NODE_ENV + ' mode!');
 });
